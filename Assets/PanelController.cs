c8ed3b378516d6f75de143c6783efa4c4c97159d
riko54321 <s2310131.suzuki.cgp@gmail.com>
2024-10-25 23:47:23 +0900

    add :sparkles: panelとaudio途中

---
+++ Assets/PanelController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PanelController : MonoBehaviour
+{
+    public GameObject optionPanel;
+    public Button optionButton;
+
+    public Button backButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        optionPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OpenPanel()
+    {
+        optionPanel.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        optionPanel.SetActive(false);
+    }
+}
