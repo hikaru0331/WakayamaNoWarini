28c11432f965010a37dc23cda29e3184c1af6caf
riko54321 <s2310131.suzuki.cgp@gmail.com>
2024-10-28 23:46:47 +0900

    add: :sparkling_heart:  audioついたにょ

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+
+    public AudioMixer audioMixer;
+    public Slider BGMSlider;
+    public Slider SESlider;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        audioMixer.GetFloat("BGM", out float bgmVolume);
+        BGMSlider.value = bgmVolume;
+        //SE
+        audioMixer.GetFloat("SE", out float seVolume);
+        SESlider.value = seVolume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
