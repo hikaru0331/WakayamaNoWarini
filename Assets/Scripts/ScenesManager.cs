0d731b240c6262783c30070e3d72c94471250ab0
riko54321 <s2310131.suzuki.cgp@gmail.com>
2024-10-31 01:12:16 +0900

    add: :sparkles: がみがみがーみー

--- Assets/Scripts/ScenesManager.cs
+++ Assets/Scripts/ScenesManager.cs
@@ -12,7 +12,7 @@ public class ScenesManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        AudioManager.instance_AudioManager.PlayBGM(0);
     }
 
     // Update is called once per frame
@@ -24,6 +24,7 @@ public class ScenesManager : MonoBehaviour
     public void OnStartButtonClicked()
     {
         SceneManager.LoadScene("SampleScene");
+        AudioManager.instance_AudioManager.PlaySE(0);
     }
 
 }
