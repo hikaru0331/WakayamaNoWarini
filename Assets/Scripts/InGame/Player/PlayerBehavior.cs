96075b5fae702dc18f4cfe7a87c8d28a9d384cc5
hikaru0331 <17731106.koga.cgp@gmail.com>
2024-11-01 20:44:01 +0900

    [add] :sparkles: ゴールフラッグの実装

--- Assets/Scripts/InGame/Player/PlayerBehavior.cs
+++ Assets/Scripts/InGame/Player/PlayerBehavior.cs
@@ -27,6 +27,8 @@ public class PlayerBehavior : MonoBehaviour
     // 着地時のイベント
     public event Action OnLandCallback;
 
+    public event Action OnGoalCallback;
+
     private System.Threading.CancellationToken token;
 
     /// <summary>
@@ -90,7 +92,7 @@ public class PlayerBehavior : MonoBehaviour
             await UniTask.WaitForSeconds(0.3f, cancellationToken: token);
 
             // ジャンプ力の最大値を0.0fから20.0fの範囲に制限
-            float clampedJumpForce = Mathf.Clamp((float)maxJumpForce, 0.0f, 21.0f);
+            float clampedJumpForce = Mathf.Clamp((float)maxJumpForce, 1.0f, 21.0f);
 
             // ジャンプの角度をラジアンに変換
             float angleInRadians = (float)jumpAngle * Mathf.Deg2Rad;
@@ -129,9 +131,18 @@ public class PlayerBehavior : MonoBehaviour
     }
 
     //GroundJudgerが地面に触れている場合のみ、isGroundedをtrueにする
-    void OnTriggerEnter2D(Collider2D other) 
+    private void OnTriggerEnter2D(Collider2D other) 
     { 
         isGrounded = true;
         OnLandCallback?.Invoke();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(other.gameObject.CompareTag("Goal"))
+        {
+            OnGoalCallback?.Invoke();
+            Debug.Log("Goal");
+        }
     }
 }
